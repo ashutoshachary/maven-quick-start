f8c6b530fb9c13dec31ba02ff3147e3785f8151d
ashutosh <ashutoshacharya908@gmail.com>
2025-01-17 15:26:31 +0530

    Changing the files

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,18 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
+
+    public void greet(){
+        List<String> args = new ArrayList<>();
+        args.add("Hello");
+
+        for (String arg : args) {
+            System.out.println("Greeting " + arg);
+        }
+      
+    }
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +22,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+    app.greet();
     }
 }
